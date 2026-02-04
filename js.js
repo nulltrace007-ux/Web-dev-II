c61c96e453d8512d46f139195c992b708aebbf19
nulltrace007-ux <nulltrace007@gmail.com>
2026-02-04 11:17:40 +0530

    Implement homework, dinner, and playground functions
    Added functions to manage homework, dinner, and playground activities with callbacks.

--- js.js
+++ js.js
@@ -1,1 +1,23 @@
-console.log("nothing")
+function finishhomework(callback){
+    console.log("starting homework....");
+    setTimeout(()=>{
+        console.log("finished homework");
+        callback();
+    },3000);
+}
+function startdinner(callback){
+    console.log("start dinner...");
+    setTimeout(() => {
+        console.log("dinner done.....");
+        callback();
+    }, 1500);
+}
+function gotoplayground(callback){
+    console.log('go to playground.....');
+    
+}
+finishhomework(() => {
+    startdinner(() => {
+        gotoplayground();
+    });
+});
